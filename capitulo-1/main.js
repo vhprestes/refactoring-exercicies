6a6d940564f282cfab8ca4cecaec3ee03e2e506c
vhprestes <vhprestes@gmail.com>
2023-06-09 19:22:50 -0300

    feat: function amountFor

--- capitulo-1/main.js
+++ capitulo-1/main.js
@@ -19,24 +19,9 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
+
+    amountFor(perf, play)
 
 
     
@@ -54,6 +39,31 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
+
+
+
+function amountFor(perf, play) {
+
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+};
 
 console.log(statement(invoice, plays))
 
--- chapter-1/main.test.js
+++
@@ -1,8 +0,0 @@
-import statement from './statement.js'
-
-
-describe('statement', () => {
-  it('should return a string', () => {
-    expect(typeof statement(invoice, plays)).toEqual('string')
-  })
-});
